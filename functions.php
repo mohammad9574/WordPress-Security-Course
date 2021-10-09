eccd7a3a1ee472c9985114b49e96b1e0d8723bd2
Mohammad Ahmadi Akbari <92208099+mohammad9574@users.noreply.github.com>
2021-10-09 17:16:03 +0330

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,5 @@
 # Remove WordPress generator meta tag completely
-function remove_generator_tag() {
-return'';
-}
+remove_action('wp_head', 'wp_generator');
 
 # Redirect Admin to Home
 add_action('template_redirect', 'bwp_template_redirect');
@@ -13,11 +11,8 @@ wp_redirect(home_url()); exit;
 }
 }
 
-# Remove Error Message from Login Page
-add_filter('login_errors', create_function('$a', "return null;"));
-
 # Change Error Message On Login Page
 function no_wordpress_errors(){
 return 'نام کاربری یا رمز عبور اشتباه است!';
 }
-add_filter('login_errors', 'no_wordpress_errors');
+add_filter( 'login_errors', 'no_wordpress_errors' );
